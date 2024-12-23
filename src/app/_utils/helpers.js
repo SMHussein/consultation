58993126f2f7233f71f2fb93d75d11ea2dc35123
SMHussein <saifabobahjat@gmail.com>
2024-12-23 20:35:32 +0300

    CHANGING SOME TITLES

--- src/app/_utils/helpers.js
+++ src/app/_utils/helpers.js
@@ -12,46 +12,44 @@ export function getMetadata(locale) {
   if (locale === "ar") {
     return {
       title: {
-        template: "ايميرج كبابليتيز للاستشارات الإدارية - %s",
+        template: "%s",
         default:
-          "ايميرج كبابليتيز للاستشارات الإدارية - تمكين الشركات السعودية من خلال حلول استشارية استراتيجية ومستدامة",
+          "ايميرج كبابليتيز للاستشارات الإدارية - حلول استشارية استراتيجية ومستدامة",
       },
       description:
-        "شركة ايميرج كبابليتيز للاستشارات الإدارية هي شركة استشارية رائدة في المملكة العربية السعودية، مكرسة لتمكين الشركات من تحقيق النمو المستدام والنجاح في الأسواق المحلية والعالمية. بفضل خبرتنا في مجالات الاستراتيجية، والتحول الرقمي، وإدارة المخاطر",
+        "شركة ايميرج كبابليتيز للاستشارات الإدارية هي شركة استشارية رائدة في المملكة العربية السعودية.",
       author: "Emerge Capabilities",
       keywords:
-        "خدمات استشارية في السعودية, حلول استشارية استراتيجية, استشارات التحول الرقمي, الحوكمة وإدارة المخاطر (GRC), النمو المستدام للأعمال, التميز التنظيمي, أبحاث السوق في السعودية, الحوكمة البيئية والاجتماعية (ESG), خدمات إدارة التغيير, تطوير الاستراتيجيات المؤسسية, تخطيط استراتيجي, استراتيجيات استدامة الأعمال, حلول إدارة المخاطر, خدمات إدارة الأداء, استشارات الابتكار, دراسات الاقتصاد المحلي, الشراكات بين القطاعين العام والخاص",
+        "خدمات استشارية في السعودية, حلول استشارية استراتيجية, استشارات التحول الرقمي, الحوكمة وإدارة المخاطر , النمو المستدام , التميز التنظيمي, أبحاث السوق في السعودية, الحوكمة, إدارة التغيير, تطوير الاستراتيجيات , تخطيط استراتيجي, استدامة الأعمال, إدارة المخاطر, إدارة الأداء, الابتكار  ",
       openGraph: {
         type: "website",
-        url: "https://ecmc-ksa.com/",
+        url: "https://www.ecmc-ksa.com",
         title:
-          "ايميرج كبابليتيز للاستشارات الإدارية - تمكين الشركات السعودية من خلال حلول استشارية استراتيجية ومستدامة",
+          "ايميرج كبابليتيز للاستشارات الإدارية - استشارية استراتيجية ومستدامة",
         description:
-          "شركة ايميرج كبابليتيز للاستشارات الإدارية هي شركة استشارية رائدة في المملكة العربية السعودية، مكرسة لتمكين الشركات من تحقيق النمو المستدام. بفضل خبرتنا في مجالات الاستراتيجية، والتحول الرقمي، وإدارة المخاطر",
+          "شركة ايميرج كبابليتيز للاستشارات الإدارية هي شركة استشارية رائدة في المملكة العربية السعودية.",
       },
-      metadataBase: new URL("https://ecmc-ksa.com"),
+      metadataBase: new URL("https://www.ecmc-ksa.com"),
     };
   }
   return {
     title: {
-      template: "ECMC - %s",
-      default:
-        "ECMC - Empowering Saudi Businesses with Strategic Consulting Solutions",
+      template: "%s",
+      default: "ECMC - Management Consulting Solutions",
     },
     description:
-      "ECMC is a leading consulting company in Saudi Arabia, dedicated to empowering businesses for sustainable growth. With expertise in strategy, digital transformation, risk management.",
+      "ECMC is a leading consulting company in Saudi Arabia. With expertise in strategy, digital transformation and risk management.",
     author: "Emerge Capabilities",
     keywords:
-      "Saudi Arabia consulting services, Strategic consulting solutions, Digital transformation consulting, Governance risk compliance (GRC), Sustainable business growth, Organizational excellence, Market research Saudi Arabia, Environmental Social Governance (ESG), Change management services, Corporate strategy development, Strategic planning consulting, Business continuity strategies, Risk management solutions, Performance management services, Innovation strategy consulting, Local economy studies consulting, Public-private sector strategies",
+      "Saudi Arabia consulting services, consulting, Digital transformation, Risk compliance, business growth, Organizational excellence, Market research Saudi Arabia, Environmental Social Governance (ESG), Corporate strategy, Strategic planning, Business continuity, Risk management, Performance management, strategy consulting, Economy consulting, strategies",
     openGraph: {
       type: "website",
-      url: "https://ecmc-ksa.com/",
-      title:
-        "ECMC - Empowering Saudi Businesses with Strategic and Sustainable Consulting Solutions",
+      url: "https://www.ecmc-ksa.com",
+      title: "ECMC - Management Consulting Solutions",
       description:
-        "ECMC is a leading consulting company in Saudi Arabia, dedicated to empowering businesses for sustainable growth and success in local and global markets. With expertise in strategy, digital transformation, risk management.",
+        "ECMC is a leading consulting company in Saudi Arabia. With expertise in strategy, digital transformation and risk management.",
     },
-    metadataBase: new URL("https://ecmc-ksa.com"),
+    metadataBase: new URL("https://www.ecmc-ksa.com"),
   };
 }
 
